6d8ea9b536c589915cdbbf86e5bf4bd020cab458
Bittu <bittukumarthakur@users.noreply.github.com>
2023-04-28 16:39:09 +0530

    Implimented a function which load a file, and run it.

--- src/apna-bash.js
+++ src/apna-bash.js
@@ -1,19 +1,43 @@
-// node apna-bash.js script.ab
-// ls - least files/directory
 const fs = require("fs");
 
-const pwd = function() {
-  return process.env.PWD;
+const getPwd = function() {
+  return pwd;
+};
+
+let pwd = process.env.PWD; // global pwd;
+
+const cd = function(path) {
+  pwd = pwd + "/" + path;
+}
+
+const ls = function() {
+  return fs.readdirSync(pwd).join(" ");
+};
+
+const loadFile = function(path) {
+  return fs.readFileSync(path, "utf-8");
+};
+
+const loadCommand = function(script) {
+  return script.trim().split("\n");
+}
+
+const executeCommand = function(commandAndArgument) {
+  const commands = {"pwd": getPwd, "ls": ls, "cd": cd}; 
+  const [command, argument] = commandAndArgument.split(" ");
+  return commands[command](argument); 
+}
+
+const display = function(lines) { 
+  console.log(lines.join("\n"));
 };
 
 const main = function() {
-  const fileName = process.argv[2];
+  const filePath = process.argv[2];
+  const script = loadFile(filePath);
+  const commands = loadCommand(script);
 
-  const command = fs.readFileSync("./src/command.ab", "utf-8");
+  display(commands.map(executeCommand));
 };
 
-
-
-
-
-console.log(main());
+main();
