a22c71c7a33310a8644b40ab5d968c99fbea06f2
Bittu <bittukumarthakur@users.noreply.github.com>
2023-04-29 08:41:49 +0530

    Removed global Pwd.

--- src/apna-bash.js
+++ src/apna-bash.js
@@ -1,43 +1,50 @@
 const fs = require("fs");
 
-const getPwd = function() {
-  return pwd;
+const getPwd = function(environment) {
+  return environment.pwd;
 };
 
-let pwd = process.env.PWD; // global pwd;
+const cd = function(environment, path) {
+  environment.pwd = environment.pwd + "/" + path;
+  return environment.pwd;
+};
 
-const cd = function(path) {
-  pwd = pwd + "/" + path;
-}
-
-const ls = function() {
-  return fs.readdirSync(pwd).join(" ");
+const ls = function(environment) {
+  return fs.readdirSync(environment.pwd).join(" ");
 };
 
 const loadFile = function(path) {
   return fs.readFileSync(path, "utf-8");
 };
 
-const loadCommand = function(script) {
+const loadLines = function(script) {
   return script.trim().split("\n");
-}
+};
 
-const executeCommand = function(commandAndArgument) {
+const executeCommand = function(environment, line) {
   const commands = {"pwd": getPwd, "ls": ls, "cd": cd}; 
-  const [command, argument] = commandAndArgument.split(" ");
-  return commands[command](argument); 
-}
+  const [command, argument] = line.split(" ");
+
+  return commands[command](environment, argument); 
+};
 
 const display = function(lines) { 
   console.log(lines.join("\n"));
 };
 
 const main = function() {
+  const pwd = process.env.PWD; 
   const filePath = process.argv[2];
   const script = loadFile(filePath);
-  const commands = loadCommand(script);
+  const lines = loadLines(script);
 
-  display(commands.map(executeCommand));
+  const context = lines.reduce( function(environment, line) {  
+    environment.output.push(executeCommand(environment, line));
+
+    return environment;
+  }, {"pwd": pwd, "output": []});
+
+  display(context.output);
 };
 
 main();
