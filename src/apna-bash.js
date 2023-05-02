b9a89decd0b791535621dd957f0408b931134380
Bittu <bittukumarthakur@users.noreply.github.com>
2023-05-02 15:24:17 +0530

    Removed some commented line.

--- src/apna-bash.js
+++ src/apna-bash.js
@@ -16,9 +16,13 @@ const execute = function(environment, line) {
   return {pwd, outputStream: outputStream.concat(output)};
 };
 
+const isNotEmpty = function(line) {
+  return line !== "";
+};
+
 const bash = function(lines) {
   const environment = {pwd: process.env.PWD, outputStream: []};
-  return lines.reduce(execute, environment).outputStream;
+  return lines.reduce(execute, environment).outputStream.filter(isNotEmpty);
 };
 
 exports.bash = bash;
--- src/commands.js
+++ src/commands.js
@@ -4,30 +4,27 @@ const pwd = function(environment) {
   return {...environment, output: environment.pwd};
 };
 
-const expandPath = function(currentPathSegment, segment) {
+const expandSegment = function(currentPathSegments, segment) {
   const home = process.env.HOME;
 
   switch(segment) {
     case "~": return home.split("/");
-    case ".": return currentPathSegment;
-    case "..": return currentPathSegment.slice(0, -1); 
-    default: return [...currentPathSegment, segment]; 
+    case ".": return currentPathSegments;
+    case "..": return currentPathSegments.slice(0, -1); 
+    default: return [...currentPathSegments, segment]; 
   };
 };
 
-// pwdPathSegments
-// pwdSegments
-// pathSegments
 const resolvePath = function(environment, path) {
-  const currentPathSegment = environment.pwd.split("/");
-  const pathSegment = path.split("/");
+  const currentPathSegments = environment.pwd.split("/");
+  const pathSegments = path.split("/");
 
-  return pathSegment.reduce(expandPath, currentPathSegment).join("/"); //review name - expandPath
+  return pathSegments.reduce(expandSegment, currentPathSegments).join("/"); 
 };
 
 const cd = function(environment, path) {
   const pwd = resolvePath(environment, path);
-  return {...environment, pwd, output: pwd};
+  return {...environment, pwd, output: ""};
 };
 
 const ls = function(environment) {
